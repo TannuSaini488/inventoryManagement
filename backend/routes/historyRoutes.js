6b3148f93436a501b793ce1dafe2f00843d3d977
Tannu Saini <tannu2005saini@gmail.com>
2025-06-30 16:36:01 +0530

    mid setup

---
+++ backend/routes/historyRoutes.js
@@ -0,0 +1,13 @@
+import express from "express";
+import HistoryModel from "../models/history_model.js";
+
+const historyRouter = express.Router();
+
+// historyRouter.get("/", async (req, res) => {
+//   const { _id } = req.body;
+
+ 
+//   const historyData = await HistoryModel.findById(_id);
+// });
+
+export default historyRouter;
